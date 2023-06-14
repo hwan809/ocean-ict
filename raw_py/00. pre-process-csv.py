23a8a25d493e22b312711d89639c37bb6158f371
hwan809 <hwan809@naver.com>
2023-06-14 18:03:00 +0900

    add : vector angle calc

--- raw_py/00. pre-process-csv.py
+++ raw_py/00. pre-process-csv.py
@@ -1,7 +1,10 @@
 import csv
+import math
 from datetime import datetime
 
 FUKSHIMA_TIME = datetime(2012, 3, 12, 15, 36)
+FUKUSHIMA_LATITUDE = 37.422972
+FUKUSHIMA_LONGITUDE = 141.032917
 
 def csv_writer(data, writer):
     writer.writerow(data)
@@ -35,9 +38,18 @@ for line in reader:
     )
     diff_seconds = (captured_datetime - FUKSHIMA_TIME).total_seconds()
 
-    # save
+    # calculate vector for circular data (angle)
+    dx = float(latitude) - FUKUSHIMA_LATITUDE 
+    dy = float(longitude) - FUKUSHIMA_LONGITUDE
+
+    vec_angle = math.atan2(dy, dx)
+
+    vec_sin = math.sin(vec_angle)
+    vec_cos = math.cos(vec_angle)
+
+    # save-
     csv_writer(
-        [diff_seconds, distance, value, latitude, longitude],
+        [diff_seconds, distance, value, latitude, longitude, vec_cos, vec_sin],
         after_writer if diff_seconds >= 0 else before_writer
     )
 
