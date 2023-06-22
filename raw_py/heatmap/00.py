5479c23088f74607d7dbf06925fb067ecd13b033
hardtack74 <geontaegim405@gmail,com>
2023-06-22 22:53:40 +0900

    test

--- raw_py/heatmap/00.py
+++ raw_py/heatmap/00.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # 데이터 불러오기
-df = pd.read_csv('ocean-ict/fukushima-dataset/after_measurements_diff_lal_long_1.csv')
+df = pd.read_csv('fukushima-dataset/after_measurements_diff_lal_long_1.csv')
 #데이터의 위도, 경도 값 제한
+
 df1 = df[(df['latitude']<=37.5)&(df['latitude']>=37.23)&(df['longitude']>=140.9)]
 #데이터의 수 제한
 df2 = df1.round(3)
